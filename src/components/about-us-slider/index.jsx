4d1d0a3f312b60a3af4075ed7663d895a4e7c49c
Dercio Maduna <76833905+dercio-codes@users.noreply.github.com>
2024-01-05 17:18:24 +0000

    feat: Updated about-us-slider and articles.js
    Updated the content and images for the about-us-slider and articles.js files.

--- src/components/about-us-slider/index.jsx
+++ src/components/about-us-slider/index.jsx
@@ -260,4 +260,26 @@ const blogPosts = [
       "Looking for ways to unwind and make the most of your weekends while on a tight budget? Check out these affordable and enjoyable activities that will help you relax, explore your interests, and connect with friends.",
     image: "/about-us.jpg",
   },
+  {
+    id: 1,
+    title: "Tips for Effective Time Management in University",
+    description:
+      "In this article, we explore valuable tips and techniques for managing your time effectively as a university student. From creating a schedule to prioritizing tasks, these strategies will help you stay organized and maximize your productivity. Whether you are juggling classes, extracurricular activities, or part-time jobs, developing good time management skills is essential for success in your university journey. Discover practical advice and actionable steps to make the most of your time and achieve your academic goals.",
+    image: "/sky.jpg",
+  },
+  {
+    id: 2,
+    title:
+      "Navigating Internship Opportunities: A Guide for University Students",
+    description:
+      "Are you interested in gaining hands-on experience through internships? This comprehensive guide provides university students with valuable insights and tips on finding and securing internship opportunities. From researching companies to preparing application materials, we cover all the essential steps to help you kickstart your career. We also delve into the benefits of internships, such as building a professional network and enhancing your resume. Whether you are a freshman or senior, this guide will equip you with the knowledge and resources to make the most of internship opportunities.",
+    image: "/young-woman-in-consultations.png",
+  },
+  {
+    id: 3,
+    title: "Building Healthy Relationships in University: Tips for Students",
+    description:
+      "Establishing meaningful connections and building a supportive community is an integral part of the university experience. In this article, we provide practical tips and advice on cultivating healthy relationships with peers, professors, and mentors. From joining student organizations to participating in networking events, we explore various opportunities for connecting with others who share similar interests and goals. Discover the benefits of building a strong support system during your university journey and learn strategies for maintaining positive relationships that can enrich your personal and academic life.",
+    image: "Health-Sciences-Banner.jpg",
+  },
 ];
--- src/pages/blog/[blog].js
+++ src/pages/blog/[blog].js
@@ -44,7 +44,7 @@ const CategoryPage = () => {
         sx={{
           height: { xs: "30vh", lg: "50vh" },
           padding: "30px",
-          backgroundImage: `url("/${currentBlogPost.image}")`,
+          backgroundImage: `url("${currentBlogPost.image}")`,
           backgroundSize: { xs: "cover", lg: "cover" },
           backgroundPosition: "left center",
         }}
@@ -177,24 +177,45 @@ export default CategoryPage;
     color: "#00e0fc",
   },
 ];
-
 const blogPosts = [
   {
     title: "10 Essential Study Tips for University Students",
     description:
-      "Studying effectively is crucial for university students to excel academically. Here are 10 essential study tips that can help you make the most of your study sessions:\n\n1. Create a Study Schedule: Develop a study schedule that includes dedicated time for each subject and breaks to avoid burnout.\n\n2. Find Your Ideal Study Environment: Experiment with different study environments to identify the one where you can focus and retain information best.\n\n3. Take Effective Notes: Use abbreviations, diagrams, and highlight important points while taking notes to aid comprehension and revision.\n\n4. Break Down Complex Topics: If you come across a challenging topic, break it down into smaller, manageable parts to better understand and remember it.\n\n5. Use Active Learning Techniques: Engage in activities like group discussions, quizzes, and problem-solving to actively engage with the material and enhance retention.\n\n6. Practice Regularly: Regular practice, especially for subjects like math and science, is key to mastering concepts and improving problem-solving skills.\n\n7. Seek Clarification: Don't hesitate to ask your professors or classmates for clarification on topics you find confusing. It's crucial to have a clear understanding of the material.\n\n8. Take Breaks: Incorporate regular breaks into your study schedule to prevent mental fatigue and maintain focus.\n\n9. Stay Organized: Keep your study materials, notes, and assignments organized to save time and avoid unnecessary stress.\n\n10. Take Care of Yourself: In addition to studying, prioritize self-care by getting enough sleep, eating well, and engaging in physical activity. A healthy body and mind contribute to academic success.\n\nBy incorporating these study tips into your routine, you'll enhance your learning experience and achieve your academic goals.",
-    image: "flu.jpg",
+      "Discover the top study tips to help you succeed in university. From effective time management to active learning techniques, these strategies will boost your productivity and academic performance.",
+    image: "/flu.jpg",
   },
   {
     title: "Navigating Life as a Freshman: A Guide for University Students",
     description:
-      "Starting university can be an exciting yet challenging phase in a student's life. Here's a comprehensive guide to help freshmen navigate this new chapter:\n\n1. Make Connections: Attend orientation events and join clubs or organizations to meet fellow students and build a support network.\n\n2. Time Management: Develop effective time management skills to balance coursework, extracurricular activities, and personal commitments.\n\n3. Seek Academic Support: Familiarize yourself with academic resources such as tutoring services, study groups, and office hours to seek assistance when needed.\n\n4. Explore Campus Resources: Take advantage of campus facilities like libraries, computer labs, and fitness centers to enhance your university experience.\n\n5. Get Involved: Participate in campus activities, events, or student government to make the most of your university's social and learning opportunities.\n\n6. Take Care of Mental Health: Prioritize your mental well-being by practicing self-care activities and seeking professional support if needed.\n\n7. Embrace Diversity: University campuses are diverse, embracing different cultures, experiences, and perspectives. Respect and learn from others' unique backgrounds.\n\n8. Manage Finances: Familiarize yourself with budgeting, student discounts, and available financial aid options to effectively manage your finances.\n\n9. Develop Study Habits: Experiment with different study techniques and find the ones that work best for you. Consistent and effective studying will lead to academic success.\n\n10. Have Fun: University is also about enjoying life. Take breaks, explore your interests, and make lifelong memories.\n\nBy following these guidelines, you'll thrive as a freshman and make the most of your university experience.",
-    image: "students-sitting.jpg",
+      "Transitioning to university can be overwhelming. This comprehensive guide offers advice on making friends, managing your workload, and getting involved in campus activities. Start your university journey on the right foot!",
+    image: "/students-sitting.jpg",
   },
   {
     title: "10 Fun and Affordable Weekend Activities for University Students",
     description:
-      "Weekends provide a great opportunity for university students to unwind, relax, and engage in enjoyable activities. Here are 10 fun and affordable weekend activities to consider:\n\n1. Explore Local Parks: Visit nearby parks, hiking trails, or botanical gardens for a refreshing and budget-friendly outdoor experience.\n\n2. Host a Movie Marathon: Organize a movie marathon with friends, where each person can contribute a film. Don't forget the popcorn!\n\n3. Volunteer in the Community: Give back to the community by volunteering at local charities, animal shelters, or environmental organizations.\n\n4. Organize a Game Night: Invite friends over for a game night filled with board games, card games, or video games for a fun and competitive evening.\n\n5. Cook a Meal Together: Take turns with your friends to cook a homemade meal and enjoy a cozy dinner at home.\n\n6. Visit Museums or Art Galleries: Many museums and art galleries offer discounted or free admission for students. Explore cultural exhibits and expand your knowledge.\n\n7. Have a Picnic: Pack a picnic basket with homemade sandwiches and snacks and enjoy a picnic in a nearby park. Don't forget a blanket!\n\n8. Explore Local Markets: Visit local farmers' markets or flea markets to discover unique crafts, fresh produce, and delicious treats.\n\n9. Attend Campus Events: Check out university-hosted events such as concerts, performances, or guest lectures. You'll get to enjoy entertainment right on campus.\n\n10. Start a Book Club: Gather a group of friends who are interested in reading and start a book club. Discuss literature and exchange book recommendations.\n\nThese activities provide a balance between relaxation, socialization, and affordable entertainment, making your weekends memorable without breaking the bank.",
-    image: "about-us.jpg",
+      "Looking for ways to unwind and make the most of your weekends while on a tight budget? Check out these affordable and enjoyable activities that will help you relax, explore your interests, and connect with friends.",
+    image: "/about-us.jpg",
+  },
+  {
+    id: 1,
+    title: "Tips for Effective Time Management in University",
+    description:
+      "In this article, we explore valuable tips and techniques for managing your time effectively as a university student. From creating a schedule to prioritizing tasks, these strategies will help you stay organized and maximize your productivity. Whether you are juggling classes, extracurricular activities, or part-time jobs, developing good time management skills is essential for success in your university journey. Discover practical advice and actionable steps to make the most of your time and achieve your academic goals.",
+    image: "/sky.jpg",
+  },
+  {
+    id: 2,
+    title:
+      "Navigating Internship Opportunities: A Guide for University Students",
+    description:
+      "Are you interested in gaining hands-on experience through internships? This comprehensive guide provides university students with valuable insights and tips on finding and securing internship opportunities. From researching companies to preparing application materials, we cover all the essential steps to help you kickstart your career. We also delve into the benefits of internships, such as building a professional network and enhancing your resume. Whether you are a freshman or senior, this guide will equip you with the knowledge and resources to make the most of internship opportunities.",
+    image: "/young-woman-in-consultations.png",
+  },
+  {
+    id: 3,
+    title: "Building Healthy Relationships in University: Tips for Students",
+    description:
+      "Establishing meaningful connections and building a supportive community is an integral part of the university experience. In this article, we provide practical tips and advice on cultivating healthy relationships with peers, professors, and mentors. From joining student organizations to participating in networking events, we explore various opportunities for connecting with others who share similar interests and goals. Discover the benefits of building a strong support system during your university journey and learn strategies for maintaining positive relationships that can enrich your personal and academic life.",
+    image: "Health-Sciences-Banner.jpg",
   },
 ];
