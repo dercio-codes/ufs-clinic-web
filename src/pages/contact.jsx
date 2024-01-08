5884b3d81fb9ce51ec5bbe88bdff6fde3fca88b0
Dercio Maduna <76833905+dercio-codes@users.noreply.github.com>
2024-01-08 11:13:32 +0000

    feat: Update contact page styling
    Updated padding and background styling of contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -32,6 +32,7 @@ export default function Contact() {
           padding: "30px",
           backgroundImage: "url('/qwaqwa-banner.jpg')",
           backgroundSize: { xs: "contain", lg: "cover" },
+          backgroundRepeat: "no-repeat",
           backgroundPosition: "left"
         }}
       />
@@ -39,7 +40,7 @@ export default function Contact() {
         sx={{
           width: "90%",
           background: "",
-          padding: "0 32px",
+          padding: { xs: "0 12px", lg: "0 32px" },
           margin: "0 auto"
         }}
       >
@@ -66,7 +67,7 @@ export default function Contact() {
               display: "flex",
               flexDirection: "column",
               alignItems: "",
-              padding: "21px 0 0 32px"
+              padding: { xs: "0 12px", lg: "21px 0 0 32px" }
             }}
           >
             <Typography component={"label"} sx={{ textAlign: "left" }}>
@@ -166,193 +167,6 @@ export default function Contact() {
               {" "}
               Get Involved{" "}
             </Button>
-          </Grid>
-
-          <Grid
-            item
-            xs={12}
-            md={2.5}
-            sx={{
-              minHeight: "70vh"
-            }}
-          >
-            <Box
-              sx={{
-                height: "",
-                width: "100%",
-                margin: "0 auto"
-              }}
-            >
-              <Box
-                sx={{
-                  height: "80px",
-                  background: "",
-                  margin: "32px 0",
-                  backgroundImage: "url('/plain.png')",
-                  backgroundSize: "contain",
-                  backgroundRepeat: "no-repeat",
-                  backgroundPosition: "center",
-                  scale: "0.7"
-                }}
-              ></Box>
-              <Box
-                sx={{
-                  height: "35vh",
-                  backgroundImage: 'url("/sky.jpg")',
-                  backgroundPosition: "right",
-                  backgroundSize: "cover",
-                  backgroundAttachment: "fixed"
-                }}
-              >
-                <Box
-                  sx={{
-                    background: "rgba(1,1,1,.5)",
-                    width: "100%",
-                    height: "100%",
-                    opacity: "0.5"
-                  }}
-                />
-              </Box>
-              <Box
-                sx={{
-                  width: "100%",
-                  margin: "0 auto",
-                  background: RED,
-                  padding: "32px 21px",
-                  borderRadius: "0 0 21px 21px"
-                }}
-              >
-                {" "}
-                <Typography
-                  sx={{
-                    fontSize: "12px",
-                    color: "white",
-                    fontWeight: "600"
-                  }}
-                >
-                  {" "}
-                  +27 51 401 9111 | info@ufs.ac.za | www.ufs.ac.za
-                </Typography>
-              </Box>
-
-              <Grid container>
-                <Grid
-                  item
-                  xs={6}
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }}
-                >
-                  <Typography
-                    sx={{
-                      fontStyle: "italic",
-                      fontSize: "10px"
-                    }}
-                  >
-                    {" "}
-                    Inspiring excellence,transforming lives through quality
-                    impact and care.{" "}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={6}
-                  sx={{
-                    height: "50px",
-                    background: "",
-                    margin: "32px 0",
-                    backgroundImage: "url('/qwaqwa-logo.jpg')",
-                    backgroundSize: "contain",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center",
-                    scale: "0.7"
-                  }}
-                ></Grid>
-              </Grid>
-            </Box>
-          </Grid>
-
-          <Grid
-            item
-            xs={12}
-            md={5.5}
-            sx={{
-              padding: "0 32px"
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: "50px",
-                color: GREEN,
-                fontWeight: "500"
-              }}
-            >
-              {" "}
-              Contact{" "}
-            </Typography>
-            <Typography sx={{ fontSize: "21px", margin: "12px 0" }}>
-              {" "}
-              Sister Mathapelo Moloi{" "}
-            </Typography>
-            <Typography sx={{ fontSize: "21px", margin: "12px 0" }}>
-              {" "}
-              T: +27 58 718 5210/5228{" "}
-            </Typography>
-            <Typography sx={{ fontSize: "21px", margin: "12px 0" }}>
-              {" "}
-              E: MoloiMA@ufs.ac.za{" "}
-            </Typography>
-
-            <Typography
-              sx={{
-                fontSize: "50px",
-                color: GREEN,
-                fontWeight: "500",
-                margin: "48px 0 32px 0"
-              }}
-            >
-              {" "}
-              Clinic Staff{" "}
-            </Typography>
-            <Typography sx={{ fontSize: "21px", margin: "12px 0" }}>
-              {" "}
-              Mathapelo Moloi (Chief Officer){" "}
-            </Typography>
-            <Typography sx={{ fontSize: "21px", margin: "12px 0" }}>
-              {" "}
-              Modise NL (Senior Officer){" "}
-            </Typography>
-            <Typography sx={{ fontSize: "21px", margin: "12px 0" }}>
-              {" "}
-              Mokhethi TF (Assisstant Offer){" "}
-            </Typography>
-            <Typography sx={{ fontSize: "21px", margin: "12px 0" }}>
-              {" "}
-              DR Mohlala (Assisstant Offer){" "}
-            </Typography>
-          </Grid>
-          <Grid
-            item
-            xs={4}
-            sx={{
-              display: "flex",
-              minHeight: { lg: "70vh" },
-              justifyContent: "flex-end",
-              alignItems: "flex-end"
-            }}
-          >
-            <Box
-              sx={{
-                height: "250px",
-                width: "100%",
-                backgroundImage: `url("/contact-image.png")`,
-                backgroundSize: "contain",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat"
-              }}
-            />
           </Grid>
         </Grid>
       </Box>
